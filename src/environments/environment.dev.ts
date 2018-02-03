7e6a59294b288d18a686cc6cf616df1f016712fe
Ben <ben@Bens-MacBook.local>
2018-02-03 07:59:44 -0500

    added environments

---
+++ src/environments/environment.dev.ts
@@ -0,0 +1,17 @@
+const apiDomain = 'http://ec2-34-228-198-79.compute-1.amazonaws.com';
+const apiPort = '8080';
+
+export const environment = {
+    production: false,
+    envName: 'local',
+    apiDomain: `${apiDomain}:${process.env.PORT} || 3000`,
+    API: {
+        LOGIN: `${apiDomain}:${apiPort}/api/login`,
+        REGISTER: `${apiDomain}:${apiPort}/api/register`,
+        IS_USERNAME_AVAILABLE: `${apiDomain}:${apiPort}/api/is-username-available`,
+        LOGOUT: `${apiDomain}:${apiPort}/api/logout`,
+        ARTICLES: `${apiDomain}:${apiPort}/api/reviews`,
+        ARTICLE: `${apiDomain}:${apiPort}/api/review`,
+        REMOVE_USER: `${apiDomain}:${apiPort}/api/user`
+    }
+};
